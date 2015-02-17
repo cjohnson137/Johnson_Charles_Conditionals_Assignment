67704afc521a9e4771fc0b277bddca0d70561261
cobaltgalaxy <CJ-MACBOOK@Charless-MacBook-Pro.local>
2015-02-17 00:15:34 -0600

    Add Files
    Placing files in Github with variables defined.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Charles Johnson
+ * Conditionals Assignment
+ * Hourly Pay Calculator
+ * SDI Section 1
+ * February 16, 2015
+ */
+
+
+    // Prompt Variables
+var yourName = prompt("What is your name?");
+var payFrequency = prompt("Do you get paid Weekly, Bi-Weekly, or Monthly?");
+var noHours = prompt("Please enter the number of hours for the pay period.");
+var perHour = prompt("Please enter your hourly pay?");
+
+
